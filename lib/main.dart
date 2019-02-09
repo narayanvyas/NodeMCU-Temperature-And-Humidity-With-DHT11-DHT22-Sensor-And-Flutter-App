57121d61148a3b7fd292b2841ba18069c9af4bcd
narayanvyas <narayanvyas87@gmail.com>
2019-02-09 17:53:04 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-String c, f, h;
+String c = '';
+String f = '';
+String h = '';
 
 BuildContext scaffoldContext;
 
@@ -40,29 +42,31 @@ class _HomeState extends State<Home> {
   String url =
       'http://192.168.1.200:80/'; //IP Address which is configured in NodeMCU Sketch
   var response;
+  bool progressIndicator = true;
 
   getSensorData() async {
     try {
-      response =
-      await http.get(url + 'celcius', headers: {"Accept": "plain/text"});
-      c = response.body + '°C';
-      print('Celcius: ' + response.body);
-      response =
-      await http.get(url + 'fahrenheit', headers: {"Accept": "plain/text"});
-      f = response.body + '°F';
-      print('Fahrenheit: ' + response.body);
-      response =
-      await http.get(url + 'humidity', headers: {"Accept": "plain/text"});
-      h = response.body + '%';
-      print('Humidity: ');
-      setState(() {
-        _status = 'Sensor Connected';
-      });
-      if (c == '200' || f == '200' || h == '200') {
+      response = await http.get(url + 'dht', headers: {"Accept": "plain/text"});
+      if (response.body == 'sensorError') {
         setState(() {
           _status = 'Sensor Not Connected';
+          c = '';
+          f = '';
+          h = '';
+          progressIndicator = false;
           displaySnackBar(context, 'Check Sensor Connections!');
         });
+      } else {
+        setState(() {
+          _status = 'Sensor Connected';
+          progressIndicator = false;
+        });
+        c = response.body.substring(0, 4) + '°C';
+        print('Celcius: ' + c);
+        f = response.body.substring(6, 10) + '°F';
+        print('Fahrenheit: ' + f);
+        h = response.body.substring(12, 16) + '%';
+        print('Humidity: ' + h);
       }
     } catch (e) {
       // If NodeMCU is not connected, it will throw error
@@ -70,6 +74,10 @@ class _HomeState extends State<Home> {
       if (this.mounted) {
         setState(() {
           _status = 'NodeMCU Not Connected';
+          c = '';
+          f = '';
+          h = '';
+          progressIndicator = false;
           displaySnackBar(context, 'Problem in WiFi Connection');
         });
       }
@@ -80,11 +88,12 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar:
-      AppBar(title: Text("Temperature And Humidity"), centerTitle: true),
+          AppBar(title: Text("Temperature And Humidity"), centerTitle: true),
       body: Builder(builder: (BuildContext context) {
         scaffoldContext = context;
         return Center(
           child: ListView(
+            padding: const EdgeInsets.only(top: 30.0),
             children: <Widget>[
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -93,16 +102,18 @@ class _HomeState extends State<Home> {
                     padding: const EdgeInsets.all(15.0),
                     child: Column(
                       children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 15.0),
+                          child: Text('Celsius'),
+                        ),
                         Image(
                           image: AssetImage('assets/celsius.png'),
                           width: 70.0,
                           height: 70.0,
                         ),
                         Container(
-                            margin: const EdgeInsets.only(top: 20.0),
-                            child: _status=='Sensor Connected'?Text(c):Container(
-                              child: CircularProgressIndicator(),
-                            )),
+                            margin: const EdgeInsets.only(top: 15.0),
+                            child: Text(c)),
                       ],
                     ),
                   ),
@@ -110,16 +121,18 @@ class _HomeState extends State<Home> {
                     padding: const EdgeInsets.all(15.0),
                     child: Column(
                       children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 15.0),
+                          child: Text('Fahrenheit'),
+                        ),
                         Image(
                           image: AssetImage('assets/fahrenheit.png'),
                           width: 70.0,
                           height: 70.0,
                         ),
                         Container(
-                            margin: const EdgeInsets.only(top: 20.0),
-                            child: _status=='Sensor Connected'?Text(f):Container(
-                              child: CircularProgressIndicator(),
-                            )),
+                            margin: const EdgeInsets.only(top: 15.0),
+                            child: Text(f)),
                       ],
                     ),
                   ),
@@ -127,28 +140,41 @@ class _HomeState extends State<Home> {
                     padding: const EdgeInsets.all(15.0),
                     child: Column(
                       children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 15.0),
+                          child: Text('Humidity'),
+                        ),
                         Image(
                           image: AssetImage('assets/humidity.png'),
                           width: 70.0,
                           height: 70.0,
                         ),
                         Container(
-                            margin: const EdgeInsets.only(top: 20.0),
-                            child: _status=='Sensor Connected'?Text(h):Container(
-                              child: CircularProgressIndicator(),
-                            )),
+                            margin: const EdgeInsets.only(top: 15.0),
+                            child: Text(h))
                       ],
                     ),
                   ),
                 ],
               ),
+              progressIndicator
+                  ? Container(
+                      margin: const EdgeInsets.only(bottom: 15.0),
+                      child: Center(child: CircularProgressIndicator()),
+                    )
+                  : Container(),
               Container(
-                padding: const EdgeInsets.all(15.0),
+                padding: const EdgeInsets.only(bottom: 15.0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     RaisedButton(
-                      onPressed: getSensorData,
+                      onPressed: () {
+                        setState(() {
+                          progressIndicator = true;
+                        });
+                        getSensorData();
+                      },
                       child: Text('Refresh'),
                     ),
                   ],
